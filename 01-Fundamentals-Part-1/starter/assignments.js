12b24cd0e1f015d8df9892146fe5a4adb6e47734
Michał Galiński <michalg359@gmail.com>
2024-02-24 14:02:33 +0100

    grinding JS

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,1 @@
+console.log('Michał galin')
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,29 @@
+// let myCurrentJob = "Teacher";
+// let myFirstJob = "Programmer";
+
+// console.log(myFirstJob) //  komentarz
+
+// let javaScripIsFun = true;
+
+// if (javaScripIsFun) console.log('Prawda')
+
+// console.log(typeof null);
+
+// let age = 30;
+
+
+// const age = 30
+
+// console.log(age)
+
+// var job = "Teacher";
+// job = "Firefighter";
+
+
+now = 2024
+const ageJonas = now - 2004
+const ageOla = now - 2005
+console.log(ageJonas, ageOla);
+
+console.log(ageJonas * 2, ageJonas / 11, 2 ** 3)
+
