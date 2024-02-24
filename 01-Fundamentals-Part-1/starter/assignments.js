c6bc466f41e82222747a21cec330e1de3f64ccf4
Michał Galiński <michalg359@gmail.com>
2024-02-24 14:41:37 +0100

    learning

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,1 +1,16 @@
-console.log('Michał galin')
+const marksMass = 78
+const marksHeight = 1.69
+
+const johnsMass = 92
+const johnsHeight = 1.95
+
+const marksBMI = marksMass / marksHeight ** 2
+const johnsBMI = johnsMass / johnsHeight ** 2
+
+const markHigherBMI = marksBMI > johnsBMI
+
+
+// console.log("Marks's BMI" + ' ' + marksBMI)
+console.log(johnsBMI)
+console.log(markHigherBMI)
+console.log("Mark's BMI" + ' ' + '(' + marksBMI + ')' + ' ' + "is higher than Johns's!" + ' ' + '(' + johnsBMI + ')')
