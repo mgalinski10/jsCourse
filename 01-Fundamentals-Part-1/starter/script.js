540a2ed825604be423bfc9f2c05efa80fb7985f3
Michał Galiński <michalg359@gmail.com>
2024-02-25 19:54:07 +0100

    another day grinding JScourse :)

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -20,10 +20,31 @@
 // job = "Firefighter";
 
 
-now = 2024
-const ageJonas = now - 2004
-const ageOla = now - 2005
-console.log(ageJonas, ageOla);
+// now = 2024
+// const ageJonas = now - 2004
+// const ageOla = now - 2005
+// console.log(ageJonas, ageOla);
 
-console.log(ageJonas * 2, ageJonas / 11, 2 ** 3)
+// console.log(ageJonas * 2, ageJonas / 11, 2 ** 3)
 
+// const firstName = 'Michal';
+// const job = 'Programmer';
+// const birthYear = 2004;
+// const year = 2024;
+
+// const michalNew = `I'm ${firstName}, a ${year - birthYear} years  old ${job}!`;
+// console.log(michalNew)
+// console.log(`string with \n\ arka`)
+
+
+// const age = 15;
+// const isOldEnough = age >= 18
+
+// if (isOldEnough) {
+//     console.log(`Michal can stat driving lesson 🚗`)
+// } else {
+//     console.log(`Michal is too young, sorry😞`)
+// }
+const inputYear = `1991`;
+console.log(inputYear + 18);
+console.log(Number(inputYear));
