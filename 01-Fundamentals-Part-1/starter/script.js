dcdf82337e1a1bb161a51f708f26d55522dadcbc
Michał Galiński <michalg359@gmail.com>
2024-02-28 10:18:22 +0100

    JS learnign Udemy

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -45,6 +45,15 @@
 // } else {
 //     console.log(`Michal is too young, sorry😞`)
 // }
-const inputYear = `1991`;
-console.log(inputYear + 18);
-console.log(Number(inputYear));
+// const inputYear = `1991`;
+// console.log(inputYear + 18);
+// console.log(Number(inputYear));
+const age = 10;
+age >= 18 ? console.log(`Lubie pix wino`) :
+    console.log(`lubie pic wode`);
+const bill = 275;
+
+/* Write your code below. Good luck! 🙂 */
+
+const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`)g
