fef2b59d78cf76c86c74657c0a5f6b4a15ce8313
Michał Galiński <michalg359@gmail.com>
2024-03-01 17:48:57 +0100

    daily study JS

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,11 +1,27 @@
 'use strict';
 
-let hasDriverLicense = false;
-const passTest = true;
+// let hasDriverLicense = false;
+// const passTest = true;
 
-function juiceProcessor(apple, banana) {
-    const juice = apple + banana
-    return console.log(`apple ${apple}, banana ${banana}`)
+// function juiceProcessor(apple, banana) {
+//     const juice = apple + banana
+//     return console.log(`apple ${apple}, banana ${banana}`)
+// }
+
+// console.log(juiceProcessor(4, 5))
+
+function calcAge1(birthYear) {
+    const age = 2024 - birthYear;
+    return age;
 }
 
-console.log(juiceProcessor(4, 5))
+function isAdult(birthYear) {
+    const age = calcAge1(birthYear); // Poprawne wywołanie funkcji calcAge1 i przechowanie wyniku w zmiennej age
+    if (age >= 18) { // Użyj zmiennej age do porównania z 18
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(isAdult(2004)); // Wywołanie funkcji isAdult z rokiem urodzenia 2004
