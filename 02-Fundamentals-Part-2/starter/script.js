b9ffaf810fd6bd920650dbdb87ac4dafed41c318
Michał Galiński <michalg359@gmail.com>
2024-03-02 11:01:31 +0100

    JSlearning

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -10,18 +10,32 @@
 
 // console.log(juiceProcessor(4, 5))
 
-function calcAge1(birthYear) {
-    const age = 2024 - birthYear;
-    return age;
-}
+// function calcAge1(birthYear) {
+//     const age = 2024 - birthYear;
+//     return age;
+// }
 
-function isAdult(birthYear) {
-    const age = calcAge1(birthYear); // Poprawne wywołanie funkcji calcAge1 i przechowanie wyniku w zmiennej age
-    if (age >= 18) { // Użyj zmiennej age do porównania z 18
-        return true;
-    } else {
-        return false;
-    }
-}
+// function isAdult(birthYear) {
+//     const age = calcAge1(birthYear); // Poprawne wywołanie funkcji calcAge1 i przechowanie wyniku w zmiennej age
+//     if (age >= 18) { // Użyj zmiennej age do porównania z 18
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
 
-console.log(isAdult(2004)); // Wywołanie funkcji isAdult z rokiem urodzenia 2004
+// console.log(isAdult(2004)); // Wywołanie funkcji isAdult z rokiem urodzenia 2004
+
+const jonas = {
+    firstName: 'Michal',
+    lastName: 'Galinski',
+    age: 2024 - 2004,
+    job: 'CEO',
+    friends: ['Michael', 'Ola', 'Steven']
+};
+
+// console.log(jonas)
+
+console.log(jonas.lastName)
+
+console.log(`Jonas has ${jonas['friends'].length} friends, and his best friend is called ${jonas['friends'][0]}.}`)
