38cb290c9389dd315600cf892635a9d868be8b34
Michał Galiński <michalg359@gmail.com>
2024-03-04 18:13:51 +0100

    learning

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,3 +1,11 @@
 'use strict';
 
-document.querySelector('.message').textContent;
+// document.querySelector('.message').textContent;
+// document.querySelector('.message').textContent = 'Correct number!';
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 20;
+
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
+document.querySelector('.check').addEventListener('click');
