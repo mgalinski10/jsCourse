e9eba22c1363a6123d42b3abc293c7b612e5f846
Michał Galiński <michalg359@gmail.com>
2024-03-04 19:06:16 +0100

    Project - GMN

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -8,4 +8,48 @@
 
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value);
-document.querySelector('.check').addEventListener('click');
+
+const secretNumber = Math.trunc(Math.random() * 20 + 1);
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+document.querySelector('.score').textContent = score;
+
+function restartGame() {
+    const secretNumber = Math.trunc(Math.random() * 20 + 1);
+    let score = 20;
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('.score').textContent = score;
+  }
+
+function isOver() {
+  if (score <=0) {
+    restartGame();
+}
+
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No guess!🚨';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct number!✅';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = 'Too high!📈';
+    score--;
+    if(score <= 0) {
+        re
+    }
+    document.querySelector('.score').textContent = score;
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = 'Too low!📉';
+    score--;
+    if (!isOver) {
+      document.querySelector('.message').textContent = 'You lost! Try again..';
+      restartGame();
+    }
+    document.querySelector('.score').textContent = score;
+  } else {
+  }
+});
