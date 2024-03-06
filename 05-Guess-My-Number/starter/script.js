5539f7b7b391afab56ad105f250b69de187c69cb
Michał Galiński <michalg359@gmail.com>
2024-03-06 12:30:48 +0100

    building mechanics in game

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -9,24 +9,14 @@
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value);
 
-const secretNumber = Math.trunc(Math.random() * 20 + 1);
-let score = 20;
-document.querySelector('.number').textContent = secretNumber;
-document.querySelector('.score').textContent = score;
-
-function restartGame() {
-    const secretNumber = Math.trunc(Math.random() * 20 + 1);
-    let score = 20;
-    document.querySelector('.number').textContent = secretNumber;
-    document.querySelector('.score').textContent = score;
-  }
-
-function isOver() {
-  if (score <=0) {
-    restartGame();
+function startGame() {
+  const secretNumber = Math.trunc(Math.random() * 20 + 1);
+  let score = 20;
+  document.querySelector('.number').textContent = secretNumber;
+  document.querySelector('.score').textContent = score;
 }
 
-
+startGame();
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(typeof guess);
@@ -38,18 +28,19 @@ document.querySelector('.check').addEventListener('click', function () {
   } else if (guess > secretNumber) {
     document.querySelector('.message').textContent = 'Too high!📈';
     score--;
-    if(score <= 0) {
-        re
-    }
     document.querySelector('.score').textContent = score;
+    if (score <= 0) {
+      document.querySelector('.message').textContent = 'You Lost! Try again..';
+      startGame();
+    }
   } else if (guess < secretNumber) {
     document.querySelector('.message').textContent = 'Too low!📉';
     score--;
-    if (!isOver) {
-      document.querySelector('.message').textContent = 'You lost! Try again..';
-      restartGame();
-    }
+
     document.querySelector('.score').textContent = score;
-  } else {
+    if (score <= 0) {
+      document.querySelector('.message').textContent = 'You Lost! Try again..';
+      startGame();
+    }
   }
 });
