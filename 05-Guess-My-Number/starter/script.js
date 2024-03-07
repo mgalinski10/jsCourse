1bab3c37cb2e2049afe7f3a85483b4497c21febf
Michał Galiński <michalg359@gmail.com>
2024-03-07 14:52:33 +0100

    working on game

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -11,8 +11,10 @@
 
 let secretNumber = Math.trunc(Math.random() * 20 + 1);
 let score = 20;
-document.querySelector('.number').textContent = '?';
-document.querySelector('.score').textContent = score;
+let highscore = 0;
+
+// document.querySelector('.number').textContent = '?';
+// document.querySelector('.score').textContent = score;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -28,6 +30,11 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('body').style.backgroundColor = 'green';
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
 
     //  When guess is too high
   } else if (guess > secretNumber) {
