7967e5ddacf70573ba3235854ba4a737467d1353
Michał Galiński <michalg359@gmail.com>
2024-03-09 17:09:49 +0100

    add features

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // Selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
@@ -36,16 +38,14 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     // Add dice to current score
     currentScore += dice;
-    document.getElementById(`current${activePlayer}`).textContent =
+    document.getElementById(`current--${activePlayer}`).textContent =
       currentScore;
   } else {
     // Switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
     activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
-});
-
-// Restart score for both players
-btnNewGame.addEventListener('click', function () {
-  current0El.textContent = 0;
-  current1El.textContent = 0;
 });
