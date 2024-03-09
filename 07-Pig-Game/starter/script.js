d2a921e4544599c10663c863c222bee16f737433
Michał Galiński <michalg359@gmail.com>
2024-03-09 11:34:42 +0100

    learning

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -18,7 +18,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEL.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -34,9 +36,11 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     // Add dice to current score
     currentScore += dice;
-    current0El.textContent = currentScore; //  CHANGE LATER
+    document.getElementById(`current${activePlayer}`).textContent =
+      currentScore;
   } else {
     // Switch to next player
+    activePlayer = activePlayer === 0 ? 1 : 0;
   }
 });
 
