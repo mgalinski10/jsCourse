0e8278489cdfeaf2b2420d2373d51e457620f014
Michał Galiński <michalg359@gmail.com>
2024-03-11 18:10:55 +0100

    Scoping in JS

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+
+  function printAge() {
+    const output = `You are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      const str = `Oh, and you're a millenial. ${firstName}`;
+      console.log(str);
+    }
+
+    console.log(millenial);
+  }
+  printAge();
+  return age;
+}
+
+const firstName = 'Michal';
+calcAge(2004);
