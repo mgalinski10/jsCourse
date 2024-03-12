cd5501db9971a7bcaa3e9202cc7863bf43fc7a60
Michał Galiński <michalg359@gmail.com>
2024-03-12 19:53:16 +0100

    arrow funtion

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,33 +1,40 @@
 'use strict';
 
-function calcAge(birthYear) {
-  const age = 2037 - birthYear;
+// function calcAge(birthYear) {
+//   const age = 2037 - birthYear;
 
-  function printAge() {
-    const output = `You are ${age}, born in ${birthYear}`;
-    console.log(output);
+//   function printAge() {
+//     const output = `You are ${age}, born in ${birthYear}`;
+//     console.log(output);
 
-    if (birthYear >= 1981 && birthYear <= 1996) {
-      var millenial = true;
-      const str = `Oh, and you're a millenial. ${firstName}`;
-      console.log(str);
-    }
+//     if (birthYear >= 1981 && birthYear <= 1996) {
+//       var millenial = true;
+//       const str = `Oh, and you're a millenial. ${firstName}`;
+//       console.log(str);
+//     }
 
-    console.log(millenial);
-  }
-  printAge();
-  return age;
-}
+//     console.log(millenial);
+//   }
+//   printAge();
+//   return age;
+// }
 
-const firstName = 'Michal';
-calcAge(2004);
+// const firstName = 'Michal';
+// calcAge(2004);
 
-// console.log(x);
+// // console.log(x);
 
-// const x = 'hello';
+// // const x = 'hello';
 
-foo(); // "Hello, world!"
+// foo(); // "Hello, world!"
 
-function foo() {
-  console.log('Hello, world!');
-}
+// function foo() {
+//   console.log('Hello, world!');
+// }
+
+const michal = {
+  firstName: 'Michal',
+  year: 2004,
+  greet: () => console.log(`Hey ${this.firstName}`),
+};
+michal.greet();
