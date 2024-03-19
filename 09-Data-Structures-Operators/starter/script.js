869c3d25b79e4f25c34a95e9dfd527be1ed69a39
Michał Galiński <michalg359@gmail.com>
2024-03-19 19:33:24 +0100

    learning

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -12,6 +12,10 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]
+  }
   openingHours: {
     thu: {
       open: 12,
@@ -32,3 +36,6 @@ const arr = [2, 3, 4];
 const a = arr[0];
 const b = arr[1];
 const c = arr[2];
+
+const [x, y, z] = arr;
+console.log(x, y, z);
